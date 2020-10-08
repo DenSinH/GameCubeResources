5215cd07c81fe64d6ad61e4cabc7415040274c3a
Dennis H <dhilhorst2000@gmail.com>
2020-10-08 19:01:48 +0200

    added some files on the DSP

---
+++ DSP/README.md
@@ -0,0 +1,4 @@
+#### DSP
+
+- Short post: http://www.retro-system.com/gc_dsp.htm
+- AX HLE post by Dolphin dev: https://blog.lse.epita.fr/2012/12/03/emulating-the-gamecube-audio-processing-in-dolphin.html
---
+++ Tests/AudioPlayer/build/oggplayer.d
@@ -0,0 +1,98 @@
+oggplayer.o: \
+ D\:/Data/CProjects/GameCubeResources/Tests/AudioPlayer/source/oggplayer.c \
+ D\:/Programs/devkitpro/libogc/include/asndlib.h \
+ D\:/Programs/devkitpro/libogc/include/gctypes.h \
+ D\:/Programs/devkitpro/portlibs/ppc/include/tremor/ivorbiscodec.h \
+ D\:/Programs/devkitpro/portlibs/ppc/include/ogg/ogg.h \
+ D\:/Programs/devkitpro/portlibs/ppc/include/ogg/os_types.h \
+ D\:/Programs/devkitpro/portlibs/ppc/include/ogg/config_types.h \
+ D\:/Programs/devkitpro/portlibs/ppc/include/tremor/ivorbisfile.h \
+ D\:/Programs/devkitpro/portlibs/ppc/include/tremor/ivorbiscodec.h \
+ D\:/Programs/devkitpro/libogc/include/gccore.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/dsp.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/aram.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/arqueue.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/lwp_queue.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/aram.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/arqmgr.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/audio.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/cache.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/card.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/cast.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/color.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/consol.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/gx_struct.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/dvd.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/disc_io.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/exi.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/gu.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/gx.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/lwp.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/gu.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/si.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/gx_struct.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/irq.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/context.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/lwp.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/mutex.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/message.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/semaphore.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/pad.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/tpl.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/gx.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/system.h \
+ D\:/Programs/devkitpro/libogc/include/gcutil.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/video.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/video_types.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/usbgecko.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/video_types.h \
+ D\:/Programs/devkitpro/libogc/include/ogc/texconv.h \
+ D\:/Data/CProjects/GameCubeResources/Tests/AudioPlayer/source/oggplayer.h
+D\:/Programs/devkitpro/libogc/include/asndlib.h:
+D\:/Programs/devkitpro/libogc/include/gctypes.h:
+D\:/Programs/devkitpro/portlibs/ppc/include/tremor/ivorbiscodec.h:
+D\:/Programs/devkitpro/portlibs/ppc/include/ogg/ogg.h:
+D\:/Programs/devkitpro/portlibs/ppc/include/ogg/os_types.h:
+D\:/Programs/devkitpro/portlibs/ppc/include/ogg/config_types.h:
+D\:/Programs/devkitpro/portlibs/ppc/include/tremor/ivorbisfile.h:
+D\:/Programs/devkitpro/portlibs/ppc/include/tremor/ivorbiscodec.h:
+D\:/Programs/devkitpro/libogc/include/gccore.h:
+D\:/Programs/devkitpro/libogc/include/ogc/dsp.h:
+D\:/Programs/devkitpro/libogc/include/ogc/aram.h:
+D\:/Programs/devkitpro/libogc/include/ogc/arqueue.h:
+D\:/Programs/devkitpro/libogc/include/ogc/lwp_queue.h:
+D\:/Programs/devkitpro/libogc/include/ogc/aram.h:
+D\:/Programs/devkitpro/libogc/include/ogc/arqmgr.h:
+D\:/Programs/devkitpro/libogc/include/ogc/audio.h:
+D\:/Programs/devkitpro/libogc/include/ogc/cache.h:
+D\:/Programs/devkitpro/libogc/include/ogc/card.h:
+D\:/Programs/devkitpro/libogc/include/ogc/cast.h:
+D\:/Programs/devkitpro/libogc/include/ogc/color.h:
+D\:/Programs/devkitpro/libogc/include/ogc/consol.h:
+D\:/Programs/devkitpro/libogc/include/ogc/gx_struct.h:
+D\:/Programs/devkitpro/libogc/include/ogc/dvd.h:
+D\:/Programs/devkitpro/libogc/include/ogc/disc_io.h:
+D\:/Programs/devkitpro/libogc/include/ogc/exi.h:
+D\:/Programs/devkitpro/libogc/include/ogc/gu.h:
+D\:/Programs/devkitpro/libogc/include/ogc/gx.h:
+D\:/Programs/devkitpro/libogc/include/ogc/lwp.h:
+D\:/Programs/devkitpro/libogc/include/ogc/gu.h:
+D\:/Programs/devkitpro/libogc/include/ogc/si.h:
+D\:/Programs/devkitpro/libogc/include/ogc/gx_struct.h:
+D\:/Programs/devkitpro/libogc/include/ogc/irq.h:
+D\:/Programs/devkitpro/libogc/include/ogc/context.h:
+D\:/Programs/devkitpro/libogc/include/ogc/lwp.h:
+D\:/Programs/devkitpro/libogc/include/ogc/mutex.h:
+D\:/Programs/devkitpro/libogc/include/ogc/message.h:
+D\:/Programs/devkitpro/libogc/include/ogc/semaphore.h:
+D\:/Programs/devkitpro/libogc/include/ogc/pad.h:
+D\:/Programs/devkitpro/libogc/include/ogc/tpl.h:
+D\:/Programs/devkitpro/libogc/include/ogc/gx.h:
+D\:/Programs/devkitpro/libogc/include/ogc/system.h:
+D\:/Programs/devkitpro/libogc/include/gcutil.h:
+D\:/Programs/devkitpro/libogc/include/ogc/video.h:
+D\:/Programs/devkitpro/libogc/include/ogc/video_types.h:
+D\:/Programs/devkitpro/libogc/include/ogc/usbgecko.h:
+D\:/Programs/devkitpro/libogc/include/ogc/video_types.h:
+D\:/Programs/devkitpro/libogc/include/ogc/texconv.h:
+D\:/Data/CProjects/GameCubeResources/Tests/AudioPlayer/source/oggplayer.h:
